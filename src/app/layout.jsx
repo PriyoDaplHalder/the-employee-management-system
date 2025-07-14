5ce1ea2294d6b5febe5ef206023779a45a099da2
indranildigitalaptech <indranil.sahu@digitalaptech.co.in>
2025-07-14 16:20:53 +0530

    add noscript

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -25,6 +25,35 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning={true}
       >
+        <noscript>
+          <h1
+            style={{
+              padding: "10px",
+              margin: "10px",
+              color: "blue",
+              textAlign: "center",
+              border: "2px solid blue",
+              borderRadius: "10px",
+              marginTop: "20px",
+            }}
+          >
+            Employee Management System
+          </h1>
+          <h2
+            style={{ padding: "20px", textAlign: "center", color: "#ff5733" }}
+          >
+            Please enable JavaScript to use this application.
+          </h2>
+          <p
+            style={{
+              textAlign: "center",
+              color: " #FFC300",
+              paddingBottom: "30px",
+            }}
+          >
+            Go to your browser settings and enable <b>Java Script</b>
+          </p>
+        </noscript>
         <MUIProvider>
           <AuthProvider>{children}</AuthProvider>
         </MUIProvider>
